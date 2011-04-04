70dd7341a30680937f7241a7ce7c7d8cf9d1b3df
Mathias Gottschlag <mgottschlag@gmail.com>
2011-04-04 20:03:33 +0200

    Added some fast integer functions.

--- include/GameMath/Math.hpp
+++ include/GameMath/Math.hpp
@@ -64,8 +64,81 @@ namespace math
 			// Cast to grab the bottom bits
 			return (int)conversion.i;
 		}
-		
-		// TODO: Fast float functions
+
+		/**
+		 * Fast function to check whether an integer is a power of two or null.
+		 * 
+		 * This function is slightly faster than isPowerOfTwo().
+		 */
+		static bool isPowerOfTwoOrNull(unsigned int n)
+		{
+			// http://graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2
+			return (n & (n - 1)) == 0;
+		}
+
+		/**
+		 * Fast function to check whether an integer is a power of two.
+		 */
+		static bool isPowerOfTwo(unsigned int n)
+		{
+			// http://graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2
+			return n && !(n & (n - 1));
+		}
+
+		/**
+		 * Fast function to calculate the logarithm of an integer if the integer
+		 * is a power of two.
+		 * 
+		 * This function is slightly faster than log2().
+		 */
+		static unsigned int log2FromPowerOfTwo(unsigned int n)
+		{
+			// http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
+			static const int MultiplyDeBruijnBitPosition2[32] =
+			{
+				0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+				31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+			};
+			return MultiplyDeBruijnBitPosition2[(uint32_t)(n * 0x077CB531U) >> 27];
+		}
+
+		/**
+		 * Returns the smallest power of two which is larger than an integer.
+		 */
+		static unsigned int nextPowerOfTwo(unsigned int n)
+		{
+			// http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+			n--;
+			n |= n >> 1;
+			n |= n >> 2;
+			n |= n >> 4;
+			n |= n >> 8;
+			n |= n >> 16;
+			n++;
+			return n;
+		}
+
+		/**
+		 * Fast function to calculate the logarithm of an integer.
+		 */
+		static unsigned int log2(unsigned int n)
+		{
+			// http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
+			static const int MultiplyDeBruijnBitPosition[32] = 
+			{
+				0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+				8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+			};
+			// First round down to one less than a power of 2 
+			n |= n >> 1; 
+			n |= n >> 2;
+			n |= n >> 4;
+			n |= n >> 8;
+			n |= n >> 16;
+			return MultiplyDeBruijnBitPosition[(uint32_t)(n * 0x07C4ACDDU) >> 27];
+		}
+
+		// TODO: Fast trigonometric functions
 	};
 }
 
